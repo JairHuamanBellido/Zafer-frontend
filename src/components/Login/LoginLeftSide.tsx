414b3e500c67f65c045802aeb66dae774b7a9617
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-09-01 20:23:37 -0500

    Añadir linter

---
+++ src/components/Login/LoginLeftSide.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginLeftSide: React.FC = () => {
+  return (
+    <div className="login-container__left-side">
+      <h1>Comunidad Gamer</h1>
+    </div>
+  );
+};
+
+export default LoginLeftSide;
---
+++ src/screen/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginLeftSide from '../components/Login/LoginLeftSide';
+
+const LoginScreen: React.FC = () => {
+  return (
+    <div className="login-container">
+      <LoginLeftSide />
+    </div>
+  );
+};
+
+export default LoginScreen;
