ed4e7d22ebf1fe15632228e7da2478dfe463081a
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-09-06 16:06:33 -0500

    Update url API

--- src/api/http.ts
+++ src/api/http.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const http = axios;
 export const httpInstance = http.create({
-  baseURL: 'http://localhost:3010/',
+  baseURL: 'https://zafergame-api.herokuapp.com/',
 });
