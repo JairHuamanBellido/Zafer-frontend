d9f3a7dd6c2cba6a1c1833a7d07fdb5947dc2c97
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-09-27 17:46:48 -0500

    Fix url in production

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   API_URL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3010'
-      : 'https://zafergame-api.herokuapp.com/',
+      : 'https://zafergame-api.herokuapp.com',
 };
